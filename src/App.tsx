977c23179dff7d6652b5d86c7046d66b8cd6dc91
Nick Jantz <thenickjantz@gmail.com>
2024-04-25 22:24:49 -0500

    shadcn mostly working

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,62 @@
 import React from 'react';
 
+import { Button } from "./components/ui/button"
+
+// import {
+// 	Select,
+// 	SelectContent,
+// 	SelectItem,
+// 	SelectTrigger,
+// 	SelectValue,
+// } from "./components/ui/select"
+
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+  } from "./components/ui/card"
+  
+
+import { Calendar } from "./components/ui/calendar"
+
 export const App = () => {
+	const [date, setDate] = React.useState<Date | undefined>(new Date())
+
 	return (
-        <h1>Hello World!</h1>
+		<>
+			<h1>Hello World!</h1>
+			<Button variant="outline">Button</Button>
+			{/* <Select>
+				<SelectTrigger className="w-[180px]">
+					<SelectValue placeholder="Theme" />
+				</SelectTrigger>
+				<SelectContent>
+					<SelectItem value="light">Light</SelectItem>
+					<SelectItem value="dark">Dark</SelectItem>
+					<SelectItem value="system">System</SelectItem>
+				</SelectContent>
+			</Select> */}
+			<Calendar
+				mode="single"
+				selected={date}
+				onSelect={setDate}
+				className="rounded-md border"
+			/>
+			<Card>
+			<CardHeader>
+				<CardTitle>Card Title</CardTitle>
+				<CardDescription>Card Description</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<p>Card Content</p>
+			</CardContent>
+			<CardFooter>
+				<p>Card Footer</p>
+			</CardFooter>
+			</Card>
+		</>
 	)
 }
